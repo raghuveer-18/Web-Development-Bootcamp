2542cf3f2b312fc32defdefab9ddc7eb0ab90227
KUMAR RAGHU VEER ROYAL AMARA <45122512+raghuveer-18@users.noreply.github.com>
2021-09-06 09:57:55 +0530

    React App

---
+++ React - KeeperApp/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>By Raghuveer</p>
+    </footer>
+  );
+}
+
+export default Footer;
